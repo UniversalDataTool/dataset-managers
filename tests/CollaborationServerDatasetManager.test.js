206241f03ea340c87bb9acbf1d7bc13296973a5d
seveibar <seveibar@gmail.com>
2020-10-10 14:52:48 -0400

    fix: add collaboration server fixture

--- tests/CollaborationServerDatasetManager.test.js
+++ tests/CollaborationServerDatasetManager.test.js
@@ -5,10 +5,14 @@ global.window.localStorage = global.localStorage
 import CollaborationServerDatasetManager from "../src/CollaborationServerDatasetManager.js"
 import test from "ava"
 import basicSuite from "./utils/basic-suite.js"
+import collaborationServer from "udt-collaboration-server"
 
 test("CollaborationServerDatasetManager Basic Suite", async (t) => {
+  const service = collaborationServer({ port: 3030 })
+  t.teardown(() => service.close())
+
   window.localStorage.app_config = JSON.stringify({
-    "collaborationServer.url": "http://localhost:3000",
+    "collaborationServer.url": "http://localhost:3030",
   })
   const dm = new CollaborationServerDatasetManager()
 
