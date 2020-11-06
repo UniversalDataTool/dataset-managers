086e1e98f202a15631aa87ea336b0e9a4aecb75a
Severin Ibarluzea <seve700@gmail.com>
2020-11-05 22:16:28 -0500

    add uploadFiles method

--- src/DatasetManager.type.js
+++ src/DatasetManager.type.js
@@ -82,4 +82,7 @@ interface DatasetManager {
 
   // We assume we can write to the dataset if not specified
   isWritable?: () => boolean;
+  
+  // If this is undefined, it's assumed the dataset manager doesn't support uploading
+  uploadFiles?: (files: Array<{ data: Buffer, destinationUrl: string }>) => Promise<{ uploadUrl: string }>
 }
